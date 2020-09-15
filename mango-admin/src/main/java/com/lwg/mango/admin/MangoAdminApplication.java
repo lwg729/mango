f76987539808fb1c362bb75987390aba00df1329
feature_liwg <liwg@hyjx.com>
2020-09-15 18:04:54 +0800

    初始化模块

---
+++ mango-admin/src/main/java/com/lwg/mango/admin/MangoAdminApplication.java
@@ -0,0 +1,13 @@
+package com.lwg.mango.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangoAdminApplication.class, args);
+    }
+
+}
